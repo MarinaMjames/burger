ddb6b055c01a4b90fee943602f48839c6f68a06e
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-07-08 12:50:26 -0400

    added example code, finished main.handlebars code

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // NODE PACKAGES
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+// var exphbs = require("express-handlebars");
+// var mysql = require("mysql");
 var methodOverride = require('method-override');
 
 // SERVER SET UP
