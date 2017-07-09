6c3923b371cc883f90d3c59e2ebde78f3bbca051
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-07-09 00:24:38 -0400

    got bobs burgers image to display correctly and centered it

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3030;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+// app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
