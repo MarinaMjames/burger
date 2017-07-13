5e16707775c468ab040aba717db188c1ce62b546
Marina James <MarinaJames@Marinas-MacBook-Air.local>
2017-07-12 21:33:43 -0400

    connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {
+} else {
 	connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "Olivia13!",
 	database: "burgers_db"
 });
-}
+};
 
 connection.connect();
 
